5def4e42683c437b8a462e1efd91118d561047c9
EABR986 <eduardoblanco198@gmail.comndgit config --global user.email eduardoblanco1987@gmail.com>
2023-02-16 12:10:47 +0100

    create the project in vite

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import './style.css'
+
 const canvas = document.getElementById('canvas');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
@@ -6,6 +8,7 @@ const colorEl = document.getElementById('color');
 const clearEl = document.getElementById('clear');
 
 const ctx = canvas.getContext('2d');
+
 
 let size = 10
 let isPressed = false
